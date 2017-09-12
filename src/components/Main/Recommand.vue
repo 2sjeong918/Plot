7c900dd9a9473d8d7cc2d05cc16c651f7d1a916d
Dain_Kang <Dain_Kang@Dain-Kangui-MacBook-Pro.local>
2017-09-12 21:44:37 +0900

    추천 1차완성

--- src/components/Main/Recommand.vue
+++ src/components/Main/Recommand.vue
@@ -1,34 +1,107 @@
-<!--Recommand Carousel --> 
 <template>
   <div id="recommand" class="recommand">
-    <ul class="rc-items container">
-      <li class="rc-item1">item1</li>
-      <li class="rc-item2">item2</li>
-      <li class="rc-item3">item3</li>
-      <li class="rc-item4">item4</li>
+    <ul class="rc-items container" :style="moveItem">
+      <li class="rc-item1":key="index" v-for="(data, index) in recommandList" >
+        <img  :src="data.thumbnail_img[0]" alt="전시 포스터 이미지">
+      </li>
     </ul>
+    <button type="button" class="carousel-button is-prev" @click.prevent="prevItem" >
+          <i class="fa fa-chevron-left" aria-hidden="true"></i>
+    </button>
+    <button type="button" class="carousel-button is-next" @click.prevent="nextItem" >
+      <i class="fa fa-chevron-right" aria-hidden="true"></i>
+    </button>
+
   </div>
 </template>
 <script>
 export default {
+  
   data() {
      return {
+       moveItem: '',
+       index: 4
+      
+       
+     }
+  },
+  computed: {
+    recommandList() {
+      return this.$store.getters.getList.slice(0,8);
+    },
+    // moveItem() {
+    //   transform: 'translateX('index')';
+    // }
+  },
 
+  mounted() {
+    this.prevItem = () => {
+      console.log('이전버튼')
+      
+        
 
+        this.moveItem = {
+          
+            transform: 'translateX(0%)' 
+            
+          }
+          
+          
+      },
+    this.nextItem = () => {
+      console.log('다음버튼')
+      
+        
+
+        this.moveItem = {
+          
+            transform: 'translateX(-100%)'
+            
+          }
+          
+          
+          
+        
+      
     }
   }
+    
 }
 </script>
 <style lang="sass" scoped>
   @import "~config"
-  
   .container
     +container(1190px 12)
+    display: flex
+    // overflow: hidden
+    background: #1e0b65
   .recommand
     margin-top: leading(2)
+    display: flex
+    position: relative
+    
   .rc-items li
     +span(3)
+    width: span(5)
     height: leading(10)
     background: #1e0b65
+    margin: 0
+    // margin-left: 2%
+    img
+      width: 300px
+      height: 100%
+      margin-left: 50%
+      transform: translateX(-50%)
+  button
+    position: absolute
+    height: 100%    
+  .is-prev
+    left: 1%
+  .is-next
+    right: 1%  
     
+      
+      
+      
+        
 </style>
